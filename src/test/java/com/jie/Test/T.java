bc0e4c3fb9c053731c4de7d95d616a8709367570
jie <2084220263@qq.com>
2023-06-08 16:44:42 +0800

    2023-06-08 16:40 保存

--- src/test/java/com/jie/Test/T.java
+++ src/test/java/com/jie/Test/T.java
@@ -1,14 +1,9 @@
 package com.jie.Test;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class T {
 //    private final Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
-    @Test
-    public void testLog(){
 
-
-    }
 }
